6cc7d0752b0d80a7ca4226291c34023d76245ef5
Matthew Walker <matthew.r.walker4@gmail.com>
2021-08-05 18:12:38 -0500

    saving intitial project set up progress

---
+++ src/main/java/com/agentlink/agentlink/AgentLinkApplication.java
@@ -0,0 +1,13 @@
+package com.agentlink.agentlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentLinkApplication.class, args);
+	}
+
+}
