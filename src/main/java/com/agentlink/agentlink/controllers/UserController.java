a710b1ee668b7b75d4c9df11c2f908056b499a84
Carrington Jones <carrington.jones47@gmail.com>
2021-08-10 11:04:15 -0500

    Feat. Created user profiles

--- src/main/java/com/agentlink/agentlink/controllers/UserController.java
+++ src/main/java/com/agentlink/agentlink/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.agentlink.agentlink.controllers;
 
 import com.agentlink.agentlink.models.User;
+import com.agentlink.agentlink.repositories.HouseRepository;
 import com.agentlink.agentlink.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +19,12 @@ import javax.validation.Valid;
 public class UserController {
     private UserRepository users;
     private PasswordEncoder passwordEncoder;
+    private HouseRepository housesDao;
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder, HouseRepository housesDao) {
         this.users = users;
         this.passwordEncoder = passwordEncoder;
+        this.housesDao = housesDao;
     }
 
     @GetMapping("/sign-up")
@@ -50,4 +54,12 @@ public class UserController {
 //        }
         return "redirect:/login";
     }
+
+    @GetMapping("/profile")
+    public String userProfile(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("houses",housesDao.findAllByUser(user));
+        return "users/profile";
+    }
+
 }
--- src/main/java/com/agentlink/agentlink/repositories/HouseRepository.java
+++ src/main/java/com/agentlink/agentlink/repositories/HouseRepository.java
@@ -1,7 +1,11 @@
 package com.agentlink.agentlink.repositories;
 
 import com.agentlink.agentlink.models.House;
+import com.agentlink.agentlink.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HouseRepository extends JpaRepository<House, Long> {
+    List<House> findAllByUser(User user);
 }
