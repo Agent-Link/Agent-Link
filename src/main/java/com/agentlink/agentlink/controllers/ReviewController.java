c449758de7229fea560ae5b42ff74cc51bb36e1f
Carrington Jones <carrington.jones47@gmail.com>
2021-08-10 14:52:15 -0500

    Worked on reviews controller and html pages

---
+++ src/main/java/com/agentlink/agentlink/controllers/ReviewController.java
@@ -0,0 +1,47 @@
+package com.agentlink.agentlink.controllers;
+
+
+import com.agentlink.agentlink.models.House;
+import com.agentlink.agentlink.models.Review;
+import com.agentlink.agentlink.models.User;
+import com.agentlink.agentlink.repositories.ReviewRepository;
+import com.agentlink.agentlink.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class ReviewController {
+
+    private ReviewRepository reviewsDao;
+    private UserRepository usersDao;
+
+    public ReviewController(ReviewRepository reviewsDao, UserRepository usersDao) {
+        this.reviewsDao = reviewsDao;
+        this.usersDao = usersDao;
+    }
+
+    @GetMapping(path = "/reviews")
+    public String getAllReviews(Model model) {
+        List<Review> reviews = reviewsDao.findAll();
+        model.addAttribute("reviews", reviews);
+        return "reviews/index";
+    }
+
+    //STILL NEED TO CREATE HTML PAGE.
+    @GetMapping("/reviews/{id}")
+    public String singleHouse(@PathVariable long id, Model model){
+        model.addAttribute("post", reviewsDao.getById(id));
+        return"reviews/show";
+    }
+
+    @GetMapping("/reviews/create")
+    public String createReview(Model model){
+        model.addAttribute("createReview", new Review());
+        return "reviews/create";
+    }
+
+}
