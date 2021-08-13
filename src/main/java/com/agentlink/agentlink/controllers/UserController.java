a2d41eaadd900a5da7b4294520fa70e7a427e97d
Carrington Jones <carrington.jones47@gmail.com>
2021-08-12 22:32:30 -0500

    Able to produce events on user profiles

--- src/main/java/com/agentlink/agentlink/controllers/UserController.java
+++ src/main/java/com/agentlink/agentlink/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.agentlink.agentlink.controllers;
 
+import com.agentlink.agentlink.models.OpenHouseEvent;
 import com.agentlink.agentlink.models.User;
 import com.agentlink.agentlink.repositories.HouseRepository;
+import com.agentlink.agentlink.repositories.OpenHouseEventRepository;
 import com.agentlink.agentlink.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,17 +16,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class UserController {
     private UserRepository users;
     private PasswordEncoder passwordEncoder;
     private HouseRepository housesDao;
+    private OpenHouseEventRepository eventsDao;
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder, HouseRepository housesDao) {
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder, HouseRepository housesDao, OpenHouseEventRepository eventsDao) {
         this.users = users;
         this.passwordEncoder = passwordEncoder;
         this.housesDao = housesDao;
+        this.eventsDao = eventsDao;
     }
 
     @GetMapping("/sign-up")
@@ -56,7 +61,9 @@ public class UserController {
     @GetMapping("/profile")
     public String userProfile(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<OpenHouseEvent> openHouseEvents = eventsDao.findAllByUser(user);
         model.addAttribute("houses",housesDao.findAllByUser(user));
+        model.addAttribute("openHouseEvents",openHouseEvents);
         return "users/profile";
     }
 
--- src/main/java/com/agentlink/agentlink/repositories/OpenHouseEventRepository.java
+++ src/main/java/com/agentlink/agentlink/repositories/OpenHouseEventRepository.java
@@ -1,7 +1,12 @@
 package com.agentlink.agentlink.repositories;
 
+import com.agentlink.agentlink.models.House;
 import com.agentlink.agentlink.models.OpenHouseEvent;
+import com.agentlink.agentlink.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OpenHouseEventRepository extends JpaRepository<OpenHouseEvent, Long> {
+    List<OpenHouseEvent> findAllByUser(User user);
 }
