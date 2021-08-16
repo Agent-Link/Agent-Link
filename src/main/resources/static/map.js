027c3ee529bfb06c819e453f746a25c7f340d22c
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-08-16 15:15:35 -0500

    small mapbox changes, for showhtml and index

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -6,6 +6,9 @@ var singleHouseState = $('#houseState').text();
 var singleHouseZipcode = $('#houseZipcode').text();
 console.log(singleHouseAddress);
 
+
+// default map load to san antonio
+
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
@@ -14,20 +17,24 @@ const map = new mapboxgl.Map({
     zoom: 8.7 // starting zoom. This zoom covers 1604 loop
 });
 
+// simple nav control
 map.addControl(new mapboxgl.NavigationControl());
 
+// creation of a single marker
 var marker = new mapboxgl.Marker({
     draggable: false,
     color: "red"
 }).setLngLat([-98.4936, 29.4241])
     .addTo(map);
 
+// pop up on the marker
 var popup = new mapboxgl.Popup()
     .setLngLat(marker.getLngLat())
     .setHTML("<h6>" + singleHouseAddress + "</h6>")
     .setMaxWidth("300px")
 marker.setPopup(popup);
 
+// for a single house, a map search for info that is compared to lng lat for the mapbox api
 geocode(singleHouseAddress, MAPBOX_ACCESS_TOKEN).then(function (info) {
     console.log(info)
     var singleHouseMarker1 = {
