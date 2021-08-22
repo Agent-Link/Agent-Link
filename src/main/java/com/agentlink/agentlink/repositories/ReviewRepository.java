5285bb1c1314f8a7f84875d23fc6d8db93df04e4
Matthew Walker <matthew.r.walker4@gmail.com>
2021-08-21 22:31:08 -0500

    removed unused code from openhouse reoi abd review repo

--- src/main/java/com/agentlink/agentlink/repositories/ReviewRepository.java
+++ src/main/java/com/agentlink/agentlink/repositories/ReviewRepository.java
@@ -2,7 +2,6 @@ package com.agentlink.agentlink.repositories;
 
 import com.agentlink.agentlink.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
