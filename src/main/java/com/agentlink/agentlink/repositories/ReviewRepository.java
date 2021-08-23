f96701271d4334a7aa480dc1bc82c0191581776e
Jordan Felan <felan.jordan94@gmail.com>
2021-08-23 11:52:43 -0500

    can now see an User rating average if they have had reviews. if they have no reviews then the line of text will not even show. so that is all good and squared away

--- src/main/java/com/agentlink/agentlink/repositories/ReviewRepository.java
+++ src/main/java/com/agentlink/agentlink/repositories/ReviewRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findAllByBuyingUser(User user);
 }
