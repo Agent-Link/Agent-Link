f083f5125a5bba2092e4b7b876bdd52b4183ffbf
Matthew Walker <matthew.r.walker4@gmail.com>
2021-08-24 22:31:39 -0500

    added applied to tab to profile

--- src/main/java/com/agentlink/agentlink/repositories/ApplicationRepository.java
+++ src/main/java/com/agentlink/agentlink/repositories/ApplicationRepository.java
@@ -2,9 +2,16 @@ package com.agentlink.agentlink.repositories;
 
 import com.agentlink.agentlink.models.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     List<Application> findApplicationsByOpenHouseEventId(long id);
+
+    List<Application> findAllByUserId(long id);
+
+    @Query("FROM Application A WHERE A.user.id = ?1 AND A.openHouseEvent.user.id <> A.user.id")
+    List<Application> findAllByUserIdWhereNotHost(long id);
+
 }
