94046d91be70207a38c35fa2596cd82850f38d55
Matthew Walker <matthew.r.walker4@gmail.com>
2021-09-08 16:27:39 -0500

    updated events applied to, to only show upcoming events sorted by date

--- src/main/java/com/agentlink/agentlink/repositories/ApplicationRepository.java
+++ src/main/java/com/agentlink/agentlink/repositories/ApplicationRepository.java
@@ -4,6 +4,7 @@ import com.agentlink.agentlink.models.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
@@ -11,7 +12,7 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 
     List<Application> findAllByUserId(long id);
 
-    @Query("FROM Application A WHERE A.user.id = ?1 AND A.openHouseEvent.user.id <> A.user.id")
-    List<Application> findAllByUserIdWhereNotHost(long id);
+    @Query("FROM Application A WHERE A.user.id = ?1 AND A.openHouseEvent.user.id <> A.user.id AND A.openHouseEvent.dateStart >= ?2 ORDER BY A.openHouseEvent.dateStart")
+    List<Application> findAllByUserIdWhereNotHostAndEventHasNotStarted(long id, Date date);
 
 }
